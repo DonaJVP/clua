272338418c9828a254a113591b8f25806ed21ba0
DonaJVP <gregoriapintos7@gmail.com>
2026-08-13 00:31:04 -0300

    Throwed clibInit.hpp

---
+++ src/clibInit.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include "lua.hpp"
+
+// In/Out library
+void LIBC__InitializeStreamLibrary(lua_Table *toSave);
+// String library
+void LIBC__InitializeStringLibrary(lua_Table *toSave);

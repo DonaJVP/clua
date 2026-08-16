fe367bcd1111e6950e24fb528fee6117283fca43
DonaJVP <gregoriapintos7@gmail.com>
2026-08-16 13:53:14 -0300

    Include Pointer for each object

--- src/clobject.hpp
+++ src/clobject.hpp
@@ -8,6 +8,7 @@ struct lua_Table;
 struct lua_ObjectT {
     lua_Table *data;
     std::string name;
+    uintptr_t Pointer;
     std::unordered_map<std::string, uint64_t> *_funcs; // I want to save this local to a 8byte slot, not 32bytes
 };
 

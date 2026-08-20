07aa24ab954892ff864c01afa8d66dfea8528edf
DonaJVP <gregoriapintos7@gmail.com>
2026-08-19 23:09:50 -0300

    Debug: Added ud2() for direct code debug

--- src/libs/clibDebugMethods.cpp
+++ src/libs/clibDebugMethods.cpp
@@ -28,10 +28,18 @@ static void calcTime(uint64_t a, uint64_t b, uint64_t c) {
     }
 }
 
+void _UD2() {
+    __asm__("ud2");
+}
+
 // Compile this function
 //#ifdef DEBUG
 void LIBC__InitializeDebugKit(lua_Table *g) {
     TString *CC = returnCompiledString("CC");
+    TString *UD2 = returnCompiledString("ud2");
     _F_ASM_NOTGUARANTEED_SETVALUE(g, returnCompiledString("CC"), lua_makeVar(((void*)calcTime), LuaFunction));
+    _F_ASM_NOTGUARANTEED_SETVALUE(g, UD2, lua_makeVar(((void*)_UD2), LuaFunction));
 }
 //#endif
+
+

d49de0ec1a5aed45f3c8f75e5f2b604c3410c2fd
DonaJVP <gregoriapintos7@gmail.com>
2026-08-20 23:34:18 -0300

    Added settings

---
+++ src/clua_settings.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+// Should it show what code is generated inside compiler?
+static bool showGeneratedCode = false;
+
+// Should export generated raw code into a file?
+static bool exportCodeToUniqueFile = false;
+static char* _ECTUF_Filename = nullptr;

63852f7fff42401b69a1113f23f8b76d9462f0b5
DonaJVP <gregoriapintos7@gmail.com>
2026-09-19 23:55:54 -0300

    Added register allocator modules (U)

--- src/clregalloc.cpp
+++ src/clregalloc.cpp
@@ -1,5 +1,11 @@
 #include "clregalloc.hpp"
+#include "cljit.hpp"
+#include "lua.hpp"
+#include <asmjit/core/operand.h>
+#include <asmjit/x86/x86builder.h>
+#include <asmjit/x86/x86operand.h>
 #include <cstdint>
+#include <stdexcept>
 #include <vector>
 
 // Register allocator for the compiler, which maybe will optimize the front outs.
@@ -35,24 +41,27 @@ CL_RegisterAllocator::CL_RegisterAllocator(void *asm_): m_asm(asm_) {
 int32_t CL_RegisterAllocator::getNextQwordNupdate() {
     int64_t c = bytes;
     bytes -= 8;
-    return c;
+    return bytes;
 }
 
 void CL_RegisterAllocator::emitCall() {
-    x86::Assembler *a = (x86::Assembler*)m_asm;
+    x86::Builder *a = (x86::Builder*)m_asm;
     for (GeneralRegister *reg: m_registers) {
         if (reg->GR_ID == x86::rdi || reg->GR_ID == x86::rsi || reg->GR_ID == x86::rdx || reg->GR_ID == x86::rax) {
             if (reg->GR_ID == x86::rax) {
                 // Deoccupy this.
-                if (reg->status[reg->name] == GR_Maintains) {
-                    if (reg->savedPtrs.find(reg->name) == reg->savedPtrs.end()) {
-                        // Alloc new qword.
-                        int32_t level = getNextQwordNupdate();
-                        reg->savedPtrs[reg->name] = level;
-                        a->mov(x86::qword_ptr(x86::rbp, level), reg->GR_ID);
-                    } else {
-                        int32_t level = reg->savedPtrs.at(reg->name);
-                        a->mov(x86::qword_ptr(x86::rbp, level), reg->GR_ID);
+                if (!reg->canOccupy) {
+                    if (reg->status[reg->name] == GR_Maintains) {
+                        if (reg->savedPtrs.find(reg->name) == reg->savedPtrs.end()) {
+                            // Alloc new qword.
+                            int32_t level = getNextQwordNupdate();
+                            reg->savedPtrs[reg->name] = level;
+                            a->mov(x86::qword_ptr(x86::rbp, level), reg->GR_ID);
+                        } else {
+                            int32_t level = reg->savedPtrs.at(reg->name);
+                            a->mov(x86::qword_ptr(x86::rbp, level), reg->GR_ID);
+                        }
+                        reg->status[reg->name] = GR_Modified;
                     }
                 }
                 reg->canOccupy = true;
@@ -71,17 +80,30 @@ void CL_RegisterAllocator::emitCall() {
                     int32_t level = reg->savedPtrs.at(reg->name);
                     a->mov(x86::qword_ptr(x86::rbp, level), reg->GR_ID);
                 }
+                reg->status[reg->name] = GR_Modified;
             }
         }
     }
 }
 
-GeneralRegister *CL_RegisterAllocator::searchRegByName(const std::string &name) { 
+GeneralRegister *CL_RegisterAllocator::searchRegByNameX(const std::string &name) {
+    if (m_registersStatus.find(name) != m_registersStatus.end()) {
+        return m_registersStatus.at(name);
+    } else {
+        return nullptr;
+    }
+}
+
+GeneralRegister *CL_RegisterAllocator::searchRegByName(const std::string &name, bool onlyReadIfMemory) { 
     if (m_registersStatus.find(name) != m_registersStatus.end()) {
         // Check their status.
         GeneralRegister *reg = m_registersStatus.at(name);
         if (reg->status[name] == GR_Modified) {
-            x86::Assembler *a = (x86::Assembler*)m_asm;
+            // If onlyReadIfMemory is set, just return a reinterpret_cast object.
+            if (onlyReadIfMemory) {
+                
+                return nullptr;
+            }
             // See if other valued already occupied this register so we can save theirs.
             if (reg->name != name) {
                 if (reg->status.at(reg->name) == GR_Maintains) {
@@ -99,6 +121,10 @@ GeneralRegister *CL_RegisterAllocator::searchRegByName(const std::string &name) 
                 }
             }
             // Restore it.
+            if (reg->savedPtrs[name] == 0) {
+                m_LuaErrorHandler->reportError(_lua_es_Illegal, 0, "Unknown ptr offset for "+name+" register, supposed to had more than 0!");
+                m_LuaErrorHandler->setFatal(true);
+            }
             a->mov(reg->GR_ID, x86::qword_ptr(x86::rbp, reg->savedPtrs[name]));
         }
         reg->uses = reg->uses + 1;
@@ -108,8 +134,43 @@ GeneralRegister *CL_RegisterAllocator::searchRegByName(const std::string &name) 
     }
 }
 
+// Used only if register are already known
+void CL_RegisterAllocator::pushToKnownRegister(const std::string &name) {
+    if (m_registersStatus.find(name) != m_registersStatus.end()) {
+        GeneralRegister *reg = m_registersStatus.at(name);
+        reg->status[name] = GR_Maintains;
+    }
+}
+
+// DEBUG LANE.
+const std::string _getRegisterName(x86::Gp K) {
+    switch (K.id()) {
+        case x86::Gp::kIdAx: return "<>AX";
+        case x86::Gp::kIdBp: return "<>BP";
+        case x86::Gp::kIdBx: return "<>BX";
+        case x86::Gp::kIdCx: return "<>CX";
+        case x86::Gp::kIdDi: return "<>DI";
+        case x86::Gp::kIdDx: return "<>DX";
+        case x86::Gp::kIdR10: return "<>10";
+        case x86::Gp::kIdR11: return "<>11";
+        case x86::Gp::kIdR12: return "<>12";
+        case x86::Gp::kIdR13: return "<>13";
+        case x86::Gp::kIdR14: return "<>14";
+        case x86::Gp::kIdR15: return "<>15";
+        case x86::Gp::kIdSi: return "<>SI";
+        case x86::Gp::kIdSp: return "<>SP";
+        case x86::Gp::kIdR8: return "<>8";
+        case x86::Gp::kIdR9: return "<>9";
+    }
+    return "<!><!>";
+}
+
 GeneralRegister *CL_RegisterAllocator::createGR(const std::string &name, bool nonExpansible, x86::Gp _preferGp) {
-    x86::Assembler *a = (x86::Assembler*)m_asm;
+    // Log
+    qlog0._log2("<+> Registering ");
+    qlog0._log2(name.c_str());
+    qlog0._log2(" as ");
+    x86::Builder *a = (x86::Builder*)m_asm;
     if (_preferGp != x86::noReg) {
         // Maybe select ONE register.
         GeneralRegister *reg_ = nullptr;
@@ -129,6 +190,7 @@ GeneralRegister *CL_RegisterAllocator::createGR(const std::string &name, bool no
                     a->mov(x86::qword_ptr(x86::rbp, level), reg_->GR_ID);
                 } else {
                     int32_t level = getNextQwordNupdate();
+                    reg_->savedPtrs[reg_->name] = level;
                     a->mov(x86::qword_ptr(x86::rbp, level), reg_->GR_ID);
                 }
             }
@@ -136,6 +198,9 @@ GeneralRegister *CL_RegisterAllocator::createGR(const std::string &name, bool no
             reg_->name = name;
             reg_->status[name] = GR_Maintains;
             reg_->uses = 0;
+            m_registersStatus[name] = reg_;
+            qlog0._log2(_getRegisterName(reg_->GR_ID).c_str());
+            qlog0._log2("\n");
             return reg_;
         } else {
             return nullptr; // Requested unknown register!
@@ -150,6 +215,8 @@ GeneralRegister *CL_RegisterAllocator::createGR(const std::string &name, bool no
             reg->uses = 0;
             reg->status[name] = GR_Maintains;
             m_registersStatus[name] = reg;
+            qlog0._log2(_getRegisterName(reg->GR_ID).c_str());
+            qlog0._log2("\n");
             return reg;
         }
     }
@@ -159,41 +226,50 @@ GeneralRegister *CL_RegisterAllocator::createGR(const std::string &name, bool no
         if (m_registersStatus.find(name) != m_registersStatus.end()) {
             // It are saved.
             GeneralRegister *r = m_registersStatus.at(name);
-            if (r->status[name] != GR_Maintains) {
-                // Oof, it is saved!..
-                GeneralRegister *rNew = createGR(name, true);
-                if (rNew != nullptr) {
-                    if (rNew->GR_ID != r->GR_ID) {
-                        // Perfect.
-                        a->mov(rNew->GR_ID, x86::qword_ptr(x86::rbp, r->savedPtrs[name]));
-                        // Set new possibilities.
-                        rNew->name = name;
-                        rNew->canOccupy = false;
-                        rNew->uses = 0;
-                        m_registersStatus[name] = rNew;
-                        return rNew;
+            try {
+                if (r->status.at(name) != GR_Maintains) {
+                    // Oof, it is saved!..
+                    GeneralRegister *rNew = createGR(name, true);
+                    if (rNew != nullptr) {
+                        if (rNew->GR_ID != r->GR_ID) {
+                            // Seek if this are saved...
+                            a->mov(rNew->GR_ID, x86::qword_ptr(x86::rbp, r->savedPtrs[name]));
+                            // Set new possibilities.
+                            rNew->name = name;
+                            rNew->canOccupy = false;
+                            rNew->uses = 0;
+                            rNew->status[name] = GR_Maintains;
+                            m_registersStatus[name] = rNew;
+                            return rNew;
+                        }
                     }
                 }
+            } catch (std::out_of_range &e) {
+                // No need to get panic
             }
         }
         // Every register is used. So we might sacrifice others.
         GeneralRegister *reg = getLeastUsedRegister();
-        reg->status[reg->name] = GR_Modified;
-        // Save data.
-        {
-            if (reg->savedPtrs.find(reg->name) == reg->savedPtrs.end()) {
-                int32_t level = getNextQwordNupdate();
-                reg->savedPtrs[reg->name] = level;
-                a->mov(x86::qword_ptr(x86::rbp, level), reg->GR_ID);
-            } else {
-                int32_t level = reg->savedPtrs.at(reg->name);
-                a->mov(x86::qword_ptr(x86::rbp, level), reg->GR_ID);
+        if (reg->status.at(reg->name) == GR_Maintains) {
+            reg->status[reg->name] = GR_Modified;
+            // Save data.
+            {
+                if (reg->savedPtrs.find(reg->name) == reg->savedPtrs.end()) {
+                    int32_t level = getNextQwordNupdate();
+                    reg->savedPtrs[reg->name] = level;
+                    a->mov(x86::qword_ptr(x86::rbp, level), reg->GR_ID);
+                } else {
+                    int32_t level = reg->savedPtrs.at(reg->name);
+                    a->mov(x86::qword_ptr(x86::rbp, level), reg->GR_ID);
+                }
             }
         }
         reg->name = name;
         reg->canOccupy = false;
         reg->uses = 0;
         reg->status[name] = GR_Maintains;
+        qlog0._log2(_getRegisterName(reg->GR_ID).c_str());
+        qlog0._log2("\n");
         return reg;
     } else {
         return nullptr;
@@ -204,6 +280,11 @@ bool CL_RegisterAllocator::destroyGR(const std::string &name) {
     if (m_registersStatus.find(name) == m_registersStatus.end())
         return false;
     m_registersStatus.at(name)->canOccupy = true;
+    qlog0._log2("<-> Destroyed ");
+    qlog0._log2(name.c_str());
+    qlog0._log2(" register; ");
+    qlog0._log2(_getRegisterName(m_registersStatus.at(name)->GR_ID).c_str());
+    qlog0._log2("\n");
     return true;
 }
 
@@ -268,7 +349,7 @@ GeneralRegister *CL_RegisterAllocator::getRegisterRAW(x86::Gp gp) {
 // Disable rdi, rsi, rdx just for a moment, and then clear.
 void CL_RegisterAllocator::occupyArgumentsRegisters(GeneralRegister *gp1, GeneralRegister *gp2, GeneralRegister *gp3) {
     std::vector<x86::Gp> _regs_{x86::rdi, x86::rsi, x86::rdx};
-    x86::Assembler *a = (x86::Assembler*)m_asm;
+    x86::Builder *a = (x86::Builder*)m_asm;
     for (x86::Gp &gp: _regs_) {
         GeneralRegister *reg = getRegisterRAW(gp);
         // Save those registers if they we're populated.
@@ -299,11 +380,8 @@ void CL_RegisterAllocator::occupyArgumentsRegisters(GeneralRegister *gp1, Genera
     }
 }
 
-// Global api...
-x86::Assembler *a = nullptr;
 
-static void _CLRA__setAssemblerObject(x86::Assembler *_a) {
-    a = _a;
+static void _CLRA__setAssemblerObject(x86::Builder *_a) {
 }
 
 static void _CLRA__movWhenNeeded(GeneralRegister *gp0, GeneralRegister *gp1) {
@@ -326,9 +404,18 @@ x86::Gp T(GeneralRegister *gp) {
     return gp->GR_ID;
 }
 
-x86::Gp S(const std::string &name) {
+x86::Gp SX(const std::string &name) {
+    GeneralRegister *reg = R->searchRegByNameX(name);
+    reg->uses = reg->uses + 1;
+    return reg->GR_ID;
+}
+
+x86::Gp S(const std::string &name, bool onlyReadIfMemory) {
     // Yeet.
-    GeneralRegister *reg = R->searchRegByName(name);
+    GeneralRegister *reg = R->searchRegByName(name, onlyReadIfMemory);
+    qlog0._log2("<##> ");
+    qlog0._log2((name+" register used as "+_getRegisterName(reg->GR_ID)).c_str());
+    qlog0._log2("\n");
     reg->uses = reg->uses + 1;
     return reg->GR_ID;
 }
--- src/clregalloc.hpp
+++ src/clregalloc.hpp
@@ -20,7 +20,7 @@ struct GeneralRegister {
     std::string name = "";
     std::unordered_map<std::string, GR_Status> status;
     asmjit::x86::Gp GR_ID = asmjit::x86::noReg;
-    _R_CONTENTS CONTENTS = _R_TRASHDATA;
+    //_R_CONTENTS CONTENTS = _R_TRASHDATA;
     bool canOccupy = true;
     uint64_t uses = 0;
     uint64_t contentId = 0; // Used in HyperAssembly
@@ -35,13 +35,16 @@ public:
     GeneralRegister *createGR(const std::string &name, bool nonExpansible = false, asmjit::x86::Gp _preferGp = asmjit::x86::noReg);
     bool destroyGR(const std::string &name); // Does nothing, just puts it to discard table.
     void destroyGR(GeneralRegister *gp); // Does nothing, just puts it to discard table.
-    GeneralRegister *searchRegByName(const std::string &name);
+    GeneralRegister *searchRegByName(const std::string &name, bool onlyReadIfMemory = false);
+    GeneralRegister *searchRegByNameX(const std::string &name);
     void emitCall();
     int32_t getBytesToAlloc() { return bytes; };
+    void localsAllocated(int32_t b) { bytes = -b-40; }
     void occupyArgumentsRegisters(GeneralRegister *gp1, GeneralRegister *gp2, GeneralRegister *gp3);
     GeneralRegister *getRegisterRAW(asmjit::x86::Gp gp);
     int32_t getNextQwordNupdate();
     bool existsAtIndex(asmjit::x86::Gp &reg);
+    void pushToKnownRegister(const std::string &name);
 private:
     int32_t bytes = -40;
     GeneralRegister *getLeastUsedRegister(); // If overflows
@@ -51,7 +54,9 @@ private:
 };
 
 asmjit::x86::Gp T(GeneralRegister *gp);
-asmjit::x86::Gp S(const std::string &name);
+asmjit::x86::Gp S(const std::string &name, bool onlyReadIfMemory = false);
+asmjit::x86::Gp SX(const std::string &name); // Only return used GP [does not restore]
+asmjit::x86::Mem SM(const std::string &name, bool &status); // Only return memory operand, but show status if failed (Already in register.)
 GeneralRegister *X(asmjit::x86::Gp &reg);
 
 static void _CLRA__movWhenNeeded(GeneralRegister *gp0, GeneralRegister *gp1);
